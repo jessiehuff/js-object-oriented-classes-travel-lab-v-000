b2b9fb7ee5305b037d3709b9ff8a923ce889b463
Jessie Huff <huffjd1@gmail.com>
2018-03-08 16:36:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Route {
   }
   blocksTravelled(){
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    return Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical) + Math.abs(eastWest.indexOf(this.beginningLocation))
+    return Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical) + Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
   }
   estimatedTime(peakHour){
     if (peakHour) {
